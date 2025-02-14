1f5b5dcc95f1c985f21153b805d0f26853f8eddb
whyxxh <rubenbeg0908@gmail.com>
2025-02-14 14:12:49 +0400

    indent

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,8 @@
 return {
-  "lervag/vimtex",
-  lazy = false,
-  init = function()
-    vim.g.vimtex_view_method = "zathura"
-  end,
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+        vim.g.vimtex_compiler_method = 'latexrun'
+        vim.g.vimtex_view_method = "zathura"
+    end,
 }
